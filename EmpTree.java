2521e4878da1bf64b0357a2eec1b9490b46848a1
Halima <201720504@ammanu.edu.jo>
2021-11-22 16:56:27 +0400

    I add  Employee.java and   Result.PNG and also I  modified:   EmpTree.java

--- EmpTree.java
+++ EmpTree.java
@@ -1,3 +1,5 @@
+//@version
+
 /*
 The Design Patterns Java Companion
 
@@ -27,23 +29,24 @@ import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
 
+
 public class EmpTree extends JFrame implements TreeSelectionListener {
-  Employee boss, marketVP, prodVP;
-
-  Employee salesMgr, advMgr;
-
-  Employee prodMgr, shipMgr;
-
+   // Attribute. 
+  Employee boss;
+  Employee marketVP;
+  Employee salesMgr;
+  Employee advMgr;
+  Employee prodMgr;
+  Employee prodVP;
+  Employee shipMgr;
   JScrollPane sp;
-
   JPanel treePanel;
-
   JTree tree;
-
   DefaultMutableTreeNode troot;
-
   JLabel cost;
-
+  
+  //@author
+  // the super class for the Window
   public EmpTree() {
     super("Employee tree");
     addWindowListener(new WindowAdapter() {
@@ -56,7 +59,8 @@ public class EmpTree extends JFrame implements TreeSelectionListener {
     setGUI();
   }
 
-  //--------------------------------------
+  //@author
+  //this method is to set the Graphical user interface.
   private void setGUI() {
     treePanel = new JPanel();
     getContentPane().add(treePanel);
@@ -79,9 +83,11 @@ public class EmpTree extends JFrame implements TreeSelectionListener {
 
   }
 
-  //------------------------------------
+  //@param 
+  //this method is to load Tree and 
+  //how the tree node is display as tree Panel
   public void loadTree(Employee topDog) {
-    DefaultMutableTreeNode troot;
+    final DefaultMutableTreeNode troot;
     troot = new DefaultMutableTreeNode(topDog.getName());
     treePanel.remove(tree);
     tree = new JTree(troot);
@@ -93,9 +99,10 @@ public class EmpTree extends JFrame implements TreeSelectionListener {
     repaint();
   }
 
-  //--------------------------------------
+   //@param 
+  //this method is for adding the node of the tree
   private void addNodes(DefaultMutableTreeNode pnode, Employee emp) {
-    DefaultMutableTreeNode node;
+     DefaultMutableTreeNode node;
 
     Enumeration e = emp.elements();
     while (e.hasMoreElements()) {
@@ -105,8 +112,9 @@ public class EmpTree extends JFrame implements TreeSelectionListener {
       addNodes(node, newEmp);
     }
   }
-
-  //--------------------------------------
+  
+  //@author
+  //this one is the implemention for Employee object.
   private void makeEmployees() {
     boss = new Employee("CEO", 200000);
     boss.add(marketVP = new Employee("Marketing VP", 100000));
@@ -115,23 +123,24 @@ public class EmpTree extends JFrame implements TreeSelectionListener {
     marketVP.add(salesMgr = new Employee("Sales Mgr", 50000));
     marketVP.add(advMgr = new Employee("Advt Mgr", 50000));
 
-    for (int i = 0; i < 5; i++)
-      salesMgr.add(new Employee("Sales " + new Integer(i).toString(),
+    for (int PrimaryCondition = 0; PrimaryCondition < 5; PrimaryCondition++)
+      salesMgr.add( new Employee ("Sales " + new Integer (PrimaryCondition).toString(),
           30000.0F + (float) (Math.random() - 0.5) * 10000));
     advMgr.add(new Employee("Secy", 20000));
 
-    prodVP.add(prodMgr = new Employee("Prod Mgr", 40000));
-    prodVP.add(shipMgr = new Employee("Ship Mgr", 35000));
-    for (int i = 0; i < 4; i++)
-      prodMgr.add(new Employee("Manuf " + new Integer(i).toString(),
+    prodVP.add( prodMgr = new Employee("Prod Mgr", 40000));
+    prodVP.add( shipMgr = new Employee("Ship Mgr", 35000));
+    for (int PrimaryCondition = 0; PrimaryCondition < 4; PrimaryCondition++)
+      prodMgr.add( new Employee("Manuf " + new Integer (PrimaryCondition).toString(),
           25000.0F + (float) (Math.random() - 0.5) * 5000));
-    for (int i = 0; i < 3; i++)
-      shipMgr.add(new Employee("ShipClrk " + new Integer(i).toString(),
-          20000.0F + (float) (Math.random() - 0.5) * 5000));
+    for (int PrimaryCondition = 0; PrimaryCondition < 3; PrimaryCondition++)
+      shipMgr.add( new Employee("ShipClrk " + new Integer(PrimaryCondition).toString(),
+          20000.0F + (float) ( Math.random() - 0.5) * 5000));
 
   }
 
-  //--------------------------------------
+  //@param
+  //the method for the tree path
   public void valueChanged(TreeSelectionEvent evt) {
     TreePath path = evt.getPath();
     String selectedTerm = path.getLastPathComponent().toString();
@@ -141,32 +150,33 @@ public class EmpTree extends JFrame implements TreeSelectionListener {
       cost.setText(new Float(emp.getSalaries()).toString());
   }
 
-  //--------------------------------------
+  //@author
+  //the main file for the EmpTree
   static public void main(String argv[]) {
     new EmpTree();
   }
 }
-
+ //@author
 class Employee {
   String name;
 
   float salary;
 
-  Vector subordinates;
+ final Vector subordinates;
 
   boolean isLeaf;
 
-  Employee parent = null;
-
-  //--------------------------------------
+   Employee parent = null;
+   //@param
+  //Construction for the Employee class [_name and _salary]
   public Employee(String _name, float _salary) {
     name = _name;
     salary = _salary;
     subordinates = new Vector();
     isLeaf = false;
   }
-
-  //--------------------------------------
+   //@param
+  //Construction override for the [_parent, _name and _salary]
   public Employee(Employee _parent, String _name, float _salary) {
     name = _name;
     salary = _salary;
@@ -174,41 +184,45 @@ class Employee {
     subordinates = new Vector();
     isLeaf = false;
   }
-
-  //--------------------------------------
+   //@param
+  //set for the leaf "Tree branches". 
   public void setLeaf(boolean b) {
     isLeaf = b; //if true, do not allow children
   }
-
-  //--------------------------------------
+   //@return
+  //get method fo for the Salary
   public float getSalary() {
     return salary;
   }
-
-  //--------------------------------------
+   
+   //@return
+  //get method fo for the ame
   public String getName() {
     return name;
   }
 
-  //--------------------------------------
+   //@return
+  //Loop for the "Tree branches" add Element. 
   public boolean add(Employee e) {
     if (!isLeaf)
       subordinates.addElement(e);
     return isLeaf; //false if unsuccessful
   }
-
-  //--------------------------------------
+  
+  //@return
+  //the remove method for the employee 
   public void remove(Employee e) {
     if (!isLeaf)
       subordinates.removeElement(e);
   }
 
-  //--------------------------------------
+  //open the subordinates for the elements 
   public Enumeration elements() {
     return subordinates.elements();
   }
-
-  //--------------------------------------
+  
+  //@return
+  //the open for the tree Panel Child 
   public Employee getChild(String s) {
     Employee newEmp = null;
 
@@ -231,12 +245,13 @@ class Employee {
         return null;
     }
   }
-
-  //--------------------------------------
+  
+   //@return
+  // cheak the Salaries
   public float getSalaries() {
     float sum = salary;
-    for (int i = 0; i < subordinates.size(); i++) {
-      sum += ((Employee) subordinates.elementAt(i)).getSalaries();
+    for (int PrimaryCondition = 0; PrimaryCondition < subordinates.size(); PrimaryCondition++) {
+      sum += ((Employee) subordinates.elementAt(PrimaryCondition)).getSalaries();
     }
     return sum;
   }
